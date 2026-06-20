99147ac885eb11d92ccceccab706a47d64921c7d
Sandeep Madiki <116255492+sandeep-madiki@users.noreply.github.com>
2026-06-20 17:39:34 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map();
+        for (const str of strs) {
+            const sign = new Array(26).fill(1);
+            for (const ch of str) {
+                sign[ch.charCodeAt() - "a".charCodeAt()]++;
+            }
+            const key = sign.join("");
+            if (!map.has(key)){
+                map.set(key, [])
+            }
+            map.get(key).push(str)
+            console.log(key)
+        }
+        
+        return [...map.values()];
+    }
+}
