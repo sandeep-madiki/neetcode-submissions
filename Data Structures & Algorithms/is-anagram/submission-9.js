a4bfa61f29ed4b1eb7bb9994bbca4f92c365492c
Sandeep Madiki <116255492+sandeep-madiki@users.noreply.github.com>
2026-06-20 11:33:36 +0530

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+        const smap = {};
+        for (let i of s) {
+            smap[i] = (smap[i] || 0) + 1;
+        }
+        for (let i of t) {
+            if (!smap.hasOwnProperty(i)) {
+                return false;
+            } else {
+                if (smap[i] === 0) return false;
+                smap[i] = smap[i] - 1;
+            }
+        }
+
+        return true;
+    }
+}
