a6ecb062e8f949d060486299fa6e273482af3b0e
Sandeep Madiki <116255492+sandeep-madiki@users.noreply.github.com>
2026-06-28 11:35:23 +0530

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.js
@@ -0,0 +1,47 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        const encodedList = []
+        for (let str of strs){
+            const length = str.length
+            const eStr = `${length}#${str}`
+            encodedList.push(eStr)
+        }
+        return encodedList.join('')
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+        const indexes = []
+        let num = ''
+        for (let i=0; i < str.length; i++){
+            const ch = str[i]
+            if (nums.includes(ch)){
+                num = num + ch
+            } else if (ch === '#' && num !== ''){
+                indexes.push([i+1, i+ Number(num)+ 1])
+                i = i + Number(num)
+                num = ''
+            } else {
+                num = ''
+            }
+        }
+
+        const res = []
+        for (let i of indexes){
+            const s = i[0]
+            const e = i[1]
+            const word = str.slice(s, e)
+            res.push(word)
+        }
+
+        return res
+    }
+}
