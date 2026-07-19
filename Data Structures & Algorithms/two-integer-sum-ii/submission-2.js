f2e9201b608340e280a7c04d03caf7b73a13f0f1
Sandeep Madiki <116255492+sandeep-madiki@users.noreply.github.com>
2026-07-19 12:56:23 +0530

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        const map = {}
+        for (let i=0; i < numbers.length; i++){
+            const n = numbers[i]
+            const remaining = target - n
+            if (map[remaining] !== undefined){
+                return [map[remaining], i + 1]
+            } else {
+                map[n] = i + 1
+            }
+        }
+
+        return []
+    }
+}
