3896db288485598b2cfaad9ba8af2ef9e3158482
Sandeep Madiki <116255492+sandeep-madiki@users.noreply.github.com>
2026-08-16 16:24:41 +0530

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let res = 0 
+        let left = 0
+        let right = heights.length - 1
+
+        while (left < right){
+            const leftEl = heights[left]
+            const rightEl = heights[right]
+            const height = Math.min(leftEl, rightEl)
+            const width = right - left
+            const area = height * width
+            res = Math.max(res, area)
+
+            if (leftEl < rightEl){
+                left++
+            } else {
+                right--
+            }
+        }
+
+        return res
+    }
+}
