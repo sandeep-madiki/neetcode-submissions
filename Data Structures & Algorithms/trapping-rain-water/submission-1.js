5e23c246bf5e357328b05b8ca8b7f4420888affe
Sandeep Madiki <116255492+sandeep-madiki@users.noreply.github.com>
2026-08-16 17:33:20 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let res = 0
+        const prefix = [height[0]]
+        for (let i=1; i<height.length; i++){
+           const max = Math.max(prefix[i-1], height[i])
+           prefix.push(max)
+        }
+        
+        const suffix = new Array(height.length)
+        suffix[height.length-1] = height[height.length-1]
+        for (let i=height.length-2; i>=0; i--){
+            const max = Math.max(height[i], suffix[i+1])
+            suffix[i] = max
+        }
+
+        for (let i=1; i<height.length-1; i++){
+           const water = Math.min(prefix[i], suffix[i]) - height[i]
+           res += Math.max(0, water)
+        }
+        return res
+    }
+}
