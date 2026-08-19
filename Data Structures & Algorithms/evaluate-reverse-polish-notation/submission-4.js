660e67f5fc3fbf2b6c611eb6e74ee78190c3c278
Sandeep Madiki <116255492+sandeep-madiki@users.noreply.github.com>
2026-08-19 16:41:34 +0530

    Add: evaluate-reverse-polish-notation - submission-4

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-4.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const stack = []
+        for (const t of tokens){
+            if (t === '+'){
+                const firstLast = stack.pop()
+                const secondLast = stack.pop()
+                const res = secondLast + firstLast
+                stack.push(res)
+            } else if (t === '-'){
+                const firstLast = stack.pop()
+                const secondLast = stack.pop()
+                const res = secondLast - firstLast
+                stack.push(res)
+            } else if (t === '*'){
+                const firstLast = stack.pop()
+                const secondLast = stack.pop()
+                const res = secondLast * firstLast
+                stack.push(res)
+            } else if (t === '/'){
+                const firstLast = stack.pop()
+                const secondLast = stack.pop()
+                const res = secondLast / firstLast
+                stack.push(Math.trunc(res))
+            } else {
+                stack.push(Number(t))
+            }
+        }
+        const res = stack.pop()
+        return Math.trunc(res)
+    }
+}
